2e1407cc6d178683282edd5be8c4f00ba36e8dc0
starikcetin <cetinsamedtarik@gmail.com>
2019-10-22 18:02:57 +0300

    it is working!! :)

--- src/gui/package_json_editor.py
+++ src/gui/package_json_editor.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as ui
 from utils.package_manager import PackageManager
 from utils.config_reader import Config
+from pathlib import Path
 
 
 class PackageJsonEditor:
@@ -8,23 +9,21 @@ class PackageJsonEditor:
         self.package_manager = package_manager
         self.json = package_manager.read()
 
-
-    def _dependency_edit_window(self):
+    def dependency_edit_window(self):
         print('dependency edit window')
         return -1
-
 
     def main_window(self):
         print('main window')
         layout = [
-            [ui.Text('Package Name', size=(15,1)),          ui.InputText(default_text=self.json.get("packageName"))],
-            [ui.Text('Display Name', size=(15,1)),          ui.InputText(default_text=self.json.get("displayName"))],
-            [ui.Text('Description', size=(15,1)),           ui.InputText(default_text=self.json.get("description"))],
-            [ui.Text('Author', size=(15,1)),                ui.InputText(default_text=self.json.get("author"))],
-            [ui.Text('Licence', size=(15,1)),               ui.InputText(default_text=self.json.get("licence"))],
-            [ui.Text('Min. Unity Version', size=(15,1)),    ui.InputText(default_text=self.json.get("unity"))],
-            [ui.Text('Dependencies', size=(15,1)),          ui.Button(button_text='Edit...', auto_size_button=True)],
-            [ui.Submit(button_text='Save'),                 ui.Cancel()]
+            [ui.Text('Package Name', size=(15, 1)), ui.InputText(default_text=self.json.get("packageName"))],
+            [ui.Text('Display Name', size=(15, 1)), ui.InputText(default_text=self.json.get("displayName"))],
+            [ui.Text('Description', size=(15, 1)), ui.InputText(default_text=self.json.get("description"))],
+            [ui.Text('Author', size=(15, 1)), ui.InputText(default_text=self.json.get("author"))],
+            [ui.Text('Licence', size=(15, 1)), ui.InputText(default_text=self.json.get("licence"))],
+            [ui.Text('Min. Unity Version', size=(15, 1)), ui.InputText(default_text=self.json.get("unity"))],
+            [ui.Text('Dependencies', size=(15, 1)), ui.Button(button_text='Edit...', auto_size_button=True)],
+            [ui.Submit(button_text='Save'), ui.Cancel()]
         ]
 
         window = ui.Window('package.json editor', layout)
@@ -32,7 +31,7 @@ class PackageJsonEditor:
         event, values = window.Read()
 
         while event is 'Edit...':
-            deps = dependency_edit_window(self)
+            deps = self.dependency_edit_window()
             event, values = window.Read()
 
         print(event)
@@ -40,7 +39,7 @@ class PackageJsonEditor:
 
 
 if __name__ == "__main__":
-    c = Config()
-    c.read()
-    pje = PackageJsonEditor(PackageManager(c.package_root_path))
+    c = Config(Path("../config.json"))
+    pm = PackageManager(c.package_root_path())
+    pje = PackageJsonEditor(pm)
     pje.main_window()
--- src/utils/package_manager.py
+++ src/utils/package_manager.py
@@ -4,8 +4,10 @@ import json
 import jsonpickle
 import utils.discover as discover
 
+
 class PackageJsonObj:
-    def initWithValues(self, package_name: str, display_name: str, unity_min_version: str, description: str, version: str, dependencies: dict):
+    def init_with_values(self, package_name: str, display_name: str, unity_min_version: str, description: str,
+                         version: str, dependencies: dict):
         self.name = package_name
         self.displayName = display_name
 
@@ -16,12 +18,12 @@ class PackageJsonObj:
         self.version = version
         self.dependencies = dependencies
 
-    def initFromDict(self, init_dict: dict):
+    def init_from_dict(self, init_dict: dict):
         for key in init_dict:
             setattr(self, key, init_dict[key])
 
     def get(self, name):
-        return getattr(self, name)
+        return getattr(self, name, '')
 
     def set(self, name, value):
         setattr(self, name, value)
@@ -30,7 +32,7 @@ class PackageJsonObj:
 class PackageManager:
     def __init__(self, package_root_path: str):
         if not os.path.exists(package_root_path):
-             print("Warning: Package root does not exist.")
+            print("Warning: Package root does not exist.")
 
         self.package_root_path = package_root_path
         self.package_json_path = os.path.join(package_root_path, "package.json")
@@ -64,7 +66,7 @@ class PackageManager:
             os.makedirs(self.package_root_path)
 
         jsonObj = PackageJsonObj()
-        jsonObj.initWithValues(package_name, display_name, unity_min_version, description, version, dependencies)
+        jsonObj.init_with_values(package_name, display_name, unity_min_version, description, version, dependencies)
         self.save(jsonObj)
 
     def refresh(self):
@@ -78,9 +80,9 @@ class PackageManager:
             except Exception as ex:
                 raise Exception("package.json file is not valid JSON (" + str(ex) + ")")
             jsonObj = PackageJsonObj()
-            jsonObj.initFromDict(json)
+            jsonObj.init_from_dict(json)
             return jsonObj
 
-    def save(self, jsonObj:PackageJsonObj):
+    def save(self, jsonObj: PackageJsonObj):
         with open(self.package_json_path, "w+") as fp:
             fp.write(json.dumps(json.loads(jsonpickle.encode(jsonObj, unpicklable=False)), indent=4, sort_keys=True))
