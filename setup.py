2c2dff42ede54cb9a2f935b0a9ac3b5f1a04c6b8
starikcetin <cetinsamedtarik@gmail.com>
2019-11-01 22:42:56 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,22 @@ setuptools.setup(
     url="https://github.com/starikcetin/git-upm-publisher",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'gup = git_upm_publisher.main:main',
+            'gup.main = git_upm_publisher.main:main',
+            'gup.config-maker = git_upm_publisher.config_maker:main',
+            'gup.package-json-creator = git_upm_publisher.package_json_creator:main',
+            'gup.package-json-updater = git_upm_publisher.package_json_updater:main',
+            'gup.upm-publisher = git_upm_publisher.upm_publisher:main',
+        ],
+    },
 )
